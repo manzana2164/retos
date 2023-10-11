fa14c90ac865bcf7951b031828eca61d4c8189b3
manzana <emiliopascutti2164@gmail.com>
2023-10-11 13:28:00 -0300

    Ejercicio finalizado en JS

---
+++ Reto-00-FizzBuzz/FizzBuzz.js
@@ -0,0 +1,14 @@
+for(let i = 1; i <= 100; i++) {
+
+    let output = '';
+  
+    if(i % 3 === 0) {
+      output += 'fizz';
+    }
+  
+    if(i % 5 === 0) {
+      output += 'buzz';
+    }
+  
+    console.log(output || i);
+  }
